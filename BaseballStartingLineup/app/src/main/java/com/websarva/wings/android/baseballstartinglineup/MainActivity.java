53466f833c6619d292a9968039516ee8d54f0828
github koro <koro1008.it@gmail.com>
2018-11-25 20:04:50 +0900

    打順ボタンの処理は記載したが、データベースから情報をとってくる処理が先に必要

--- BaseballStartingLineup/app/src/main/java/com/websarva/wings/android/baseballstartinglineup/MainActivity.java
+++ BaseballStartingLineup/app/src/main/java/com/websarva/wings/android/baseballstartinglineup/MainActivity.java
@@ -2,9 +2,11 @@ package com.websarva.wings.android.baseballstartinglineup;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     //スタメンタイトル
     TextView title;
     //各打順の数字配列
-    int numbers[] = new int[19];
+    int numbers[] = new int[20];
     //グローバル変数i（データベースへの登録・検索で使う）
     int i = 0;
     //サブオーダー選択時に+10される
@@ -81,12 +83,72 @@ public class MainActivity extends AppCompatActivity {
         position8 = findViewById(R.id.position8);
         position9 = findViewById(R.id.position9);
         //打順配列に打順番号入れる(1~19番)
-        for(int i = 0;i < 19;i++){
-            numbers[i] = i + 1;
+        for(int i = 1;i < 20;i++){
+            numbers[i] = i;
         }
         //スピナー紐付け
         spinner = findViewById(R.id.position);
         //EditText入力不可に
         etName.setEnabled(false);
+
+//        TODO データベースから引っ張ってきて表示するメソッドorないなら空情報を配列に入れる
     }
+
+    //以下１〜９番の打順ボタン処理⬇
+    public void onClick1(View view){
+        commonMethod(1);
+    }
+    public void onClick2(View view){
+        commonMethod(2);
+    }
+    public void onClick3(View view){
+        commonMethod(3);
+    }
+    public void onClick4(View view){
+        commonMethod(4);
+    }
+    public void onClick5(View view){
+        commonMethod(5);
+    }
+    public void onClick6(View view){
+        commonMethod(6);
+    }
+    public void onClick7(View view){
+        commonMethod(7);
+    }
+    public void onClick8(View view) {
+        commonMethod(8);
+    }
+    public void onClick9(View view){
+        commonMethod(9);
+    }
+    //打順ボタン共通メソッド（打順・登録状態表示、EditText・登録/クリアボタンの有効化、データベース用の数字登録）
+    public void commonMethod(int j){
+        //numbersは表示打順のためkを反映させない
+        String number = String.valueOf(j);
+        tvSelectNum.setText(number);
+        //下記メソッド使用
+        setSpinner(spinner,positions[j + k]);
+        etName.setText(names[j + k]);
+        if(etName.getText().toString().equals("-----")){
+            etName.setText("");
+        }
+        etName.setEnabled(true);
+        record.setEnabled(true);
+        clear.setEnabled(true);
+        i = j;
+    }
+    //文字列からスピナーをセットするメソッド（上記メソッドで使用）
+    public void setSpinner(Spinner spinner,String position){
+        SpinnerAdapter adapter = spinner.getAdapter();
+        int index = 0;
+        for(int i = 0; i < adapter.getCount(); i++){
+            if(adapter.getItem(i).equals(position)){
+                index = i;
+                break;
+            }
+        }
+        spinner.setSelection(index);
+    }
+
 }
