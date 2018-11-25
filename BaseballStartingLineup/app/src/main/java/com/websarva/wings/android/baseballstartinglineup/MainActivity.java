dfc6b79fca6dd01ec2fd89f397e55ddb5c70043f
github koro <koro1008.it@gmail.com>
2018-11-25 19:35:09 +0900

    フィールド用意（findViewIdも）

--- BaseballStartingLineup/app/src/main/java/com/websarva/wings/android/baseballstartinglineup/MainActivity.java
+++ BaseballStartingLineup/app/src/main/java/com/websarva/wings/android/baseballstartinglineup/MainActivity.java
@@ -1,13 +1,92 @@
 package com.websarva.wings.android.baseballstartinglineup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //選択した打順
+    TextView tvSelectNum;
+    //入力欄
+    EditText etName;
+    //登録ボタン
+    Button record;
+    //スタメンタイトル
+    TextView title;
+    //各打順の数字配列
+    int numbers[] = new int[19];
+    //グローバル変数i（データベースへの登録・検索で使う）
+    int i = 0;
+    //サブオーダー選択時に+10される
+    int k = 0;
+    //各打順の名前
+    TextView name1;
+    TextView name2;
+    TextView name3;
+    TextView name4;
+    TextView name5;
+    TextView name6;
+    TextView name7;
+    TextView name8;
+    TextView name9;
+    //スピナーオブジェクト
+    Spinner spinner;
+    //クリアボタン（現在上部に入力中のものを未入力状態に戻す（選択打順も））
+    Button clear;
+    //各打順のポジション
+    TextView position1;
+    TextView position2;
+    TextView position3;
+    TextView position4;
+    TextView position5;
+    TextView position6;
+    TextView position7;
+    TextView position8;
+    TextView position9;
+    //各打順の名前,ポジション用配列
+    String[] names;
+    String[] positions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //上記のグローバルフィールド紐付け
+        tvSelectNum = findViewById(R.id.selectNum);
+        etName = findViewById(R.id.etName);
+        record = findViewById(R.id.record);
+        clear = findViewById(R.id.clear);
+        title = findViewById(R.id.title);
+        name1 = findViewById(R.id.name1);
+        name2 = findViewById(R.id.name2);
+        name3 = findViewById(R.id.name3);
+        name4 = findViewById(R.id.name4);
+        name5 = findViewById(R.id.name5);
+        name6 = findViewById(R.id.name6);
+        name7 = findViewById(R.id.name7);
+        name8 = findViewById(R.id.name8);
+        name9 = findViewById(R.id.name9);
+        position1 = findViewById(R.id.position1);
+        position2 = findViewById(R.id.position2);
+        position3 = findViewById(R.id.position3);
+        position4 = findViewById(R.id.position4);
+        position5 = findViewById(R.id.position5);
+        position6 = findViewById(R.id.position6);
+        position7 = findViewById(R.id.position7);
+        position8 = findViewById(R.id.position8);
+        position9 = findViewById(R.id.position9);
+        //打順配列に打順番号入れる(1~19番)
+        for(int i = 0;i < 19;i++){
+            numbers[i] = i + 1;
+        }
+        //スピナー紐付け
+        spinner = findViewById(R.id.position);
+        //EditText入力不可に
+        etName.setEnabled(false);
     }
 }
