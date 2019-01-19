62ec47184e3a7ddce2c9c6887dbda760e108af2e
github koro <koro1008.it@gmail.com>
2019-01-19 17:37:47 +0900

    字数によってフォントサイズ変更(DHのオーダーのみ)

--- BaseballStartingLineup/app/src/main/java/com/websarva/wings/android/baseballstartinglineup/LineupDhFragment.java
+++ BaseballStartingLineup/app/src/main/java/com/websarva/wings/android/baseballstartinglineup/LineupDhFragment.java
@@ -13,32 +13,11 @@ public class LineupDhFragment extends Fragment {
 
     public static Bundle args;
 
-    //各打順の名前
-    TextView name1;
-    TextView name2;
-    TextView name3;
-    TextView name4;
-    TextView name5;
-    TextView name6;
-    TextView name7;
-    TextView name8;
-    TextView name9;
-    TextView nameP;
+    private String[] namesDH_fromTop = new String[11];
+    private String[] positionsDH_fromTop = new String[10];
 
-    //各打順のポジション
-    TextView position1;
-    TextView position2;
-    TextView position3;
-    TextView position4;
-    TextView position5;
-    TextView position6;
-    TextView position7;
-    TextView position8;
-    TextView position9;
-    TextView positionP;
-
-    private String[] namesDH = new String[11];
-    private String[] positionsDH = new String[10];
+    private TextView[] namesDH_local = new TextView[11];
+    private TextView[] positionsDH_local = new TextView[10];
 
 
     public static LineupDhFragment newInstance(String[] names,String[] positions){
@@ -64,107 +43,73 @@ public class LineupDhFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_lineup_dh,container,false);
 
-        name1 = v.findViewById(R.id.name1DH);
-        name2 = v.findViewById(R.id.name2DH);
-        name3 = v.findViewById(R.id.name3DH);
-        name4 = v.findViewById(R.id.name4DH);
-        name5 = v.findViewById(R.id.name5DH);
-        name6 = v.findViewById(R.id.name6DH);
-        name7 = v.findViewById(R.id.name7DH);
-        name8 = v.findViewById(R.id.name8DH);
-        name9 = v.findViewById(R.id.name9DH);
-        nameP = v.findViewById(R.id.nameP);
-        position1 = v.findViewById(R.id.position1DH);
-        position2 = v.findViewById(R.id.position2DH);
-        position3 = v.findViewById(R.id.position3DH);
-        position4 = v.findViewById(R.id.position4DH);
-        position5 = v.findViewById(R.id.position5DH);
-        position6 = v.findViewById(R.id.position6DH);
-        position7 = v.findViewById(R.id.position7DH);
-        position8 = v.findViewById(R.id.position8DH);
-        position9 = v.findViewById(R.id.position9DH);
-        positionP = v.findViewById(R.id.positionP);
+        namesDH_local[1] = v.findViewById(R.id.name1DH);
+        namesDH_local[2] = v.findViewById(R.id.name2DH);
+        namesDH_local[3] = v.findViewById(R.id.name3DH);
+        namesDH_local[4] = v.findViewById(R.id.name4DH);
+        namesDH_local[5] = v.findViewById(R.id.name5DH);
+        namesDH_local[6] = v.findViewById(R.id.name6DH);
+        namesDH_local[7] = v.findViewById(R.id.name7DH);
+        namesDH_local[8] = v.findViewById(R.id.name8DH);
+        namesDH_local[9] = v.findViewById(R.id.name9DH);
+        namesDH_local[10] = v.findViewById(R.id.nameP);
+        positionsDH_local[1] = v.findViewById(R.id.position1DH);
+        positionsDH_local[2] = v.findViewById(R.id.position2DH);
+        positionsDH_local[3] = v.findViewById(R.id.position3DH);
+        positionsDH_local[4] = v.findViewById(R.id.position4DH);
+        positionsDH_local[5] = v.findViewById(R.id.position5DH);
+        positionsDH_local[6] = v.findViewById(R.id.position6DH);
+        positionsDH_local[7] = v.findViewById(R.id.position7DH);
+        positionsDH_local[8] = v.findViewById(R.id.position8DH);
+        positionsDH_local[9] = v.findViewById(R.id.position9DH);
 
-        namesDH = getArguments().getStringArray(NAMES);
-        positionsDH = getArguments().getStringArray(POSITONS);
+        namesDH_fromTop = getArguments().getStringArray(NAMES);
+        positionsDH_fromTop = getArguments().getStringArray(POSITONS);
 
-        setInfo(namesDH,positionsDH);
-
+        setInfo(namesDH_fromTop, positionsDH_fromTop);
 
         return v;
     }
 
 
-
     public void setInfo(String[] names,String[] positions ){
 
-        // レイアウトにいっせい表示
-        name1.setText(names[1]);
-        name2.setText(names[2]);
-        name3.setText(names[3]);
-        name4.setText(names[4]);
-        name5.setText(names[5]);
-        name6.setText(names[6]);
-        name7.setText(names[7]);
-        name8.setText(names[8]);
-        name9.setText(names[9]);
-        nameP.setText(names[10]);
+        // レイアウト表示
+        for(int i = 1;i < 10;i++){
+            namesDH_local[i].setText(names[i]);
+            positionsDH_local[i].setText(positions[i]);
+            changeTextSize(namesDH_local[i]);
+        }
+        namesDH_local[10].setText(names[10]);
+        changeTextSize(namesDH_local[10]);
 
-        position1.setText(positions[1]);
-        position2.setText(positions[2]);
-        position3.setText(positions[3]);
-        position4.setText(positions[4]);
-        position5.setText(positions[5]);
-        position6.setText(positions[6]);
-        position7.setText(positions[7]);
-        position8.setText(positions[8]);
-        position9.setText(positions[9]);
+    }
+
+    public void changeTextSize(TextView textView){
+        // 字数取得
+        int lengthOfText = textView.length();
+
+        // 字数によって文字サイズ変更
+        if(lengthOfText < 12){
+            textView.setTextSize(24);
+        } else if(lengthOfText < 15){
+            textView.setTextSize(20);
+        } else if(lengthOfText < 17){
+            textView.setTextSize(18);
+        } else {
+            textView.setTextSize(16);
+        }
 
     }
 
     // 特定の打順の内容変更
     public void textChange(int num,String position,String name){
 
-        switch (num){
-            case 1:
-                position1.setText(position);
-                name1.setText(name);
-                break;
-            case 2:
-                position2.setText(position);
-                name2.setText(name);
-                break;
-            case 3:
-                position3.setText(position);
-                name3.setText(name);
-                break;
-            case 4:
-                position4.setText(position);
-                name4.setText(name);
-                break;
-            case 5:
-                position5.setText(position);
-                name5.setText(name);
-                break;
-            case 6:
-                position6.setText(position);
-                name6.setText(name);
-                break;
-            case 7:
-                position7.setText(position);
-                name7.setText(name);
-                break;
-            case 8:
-                position8.setText(position);
-                name8.setText(name);
-                break;
-            case 9:
-                position9.setText(position);
-                name9.setText(name);
-                break;
-            case 10:
-                nameP.setText(name);
-                break;
+        namesDH_local[num].setText(name);
+        changeTextSize(namesDH_local[num]);
+
+        if(num < 10){
+            positionsDH_local[num].setText(position);
         }
     }
 
